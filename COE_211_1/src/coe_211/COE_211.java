8c4312653fbb60faead68a8127076dd3f6c5a10a
haidar hamdan <haidar.hamdan03@gmail.com>
2022-02-19 11:56:34 +0200

    COE_211

---
+++ COE_211_1/src/coe_211/COE_211.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coe_211;
+
+/**
+ *
+ * @author haida
+ */
+public class COE_211 {
+    public static void main(String[] args) {
+      System.out.print("Hello from GitHub!");
+    }
+    
+}
